49e3aebdb5369d4735f8a3235bac001e24345530
w0lwin <teo.bacher@hotmail.com>
2025-10-30 14:14:38 +0100

    feat: :sparkles: rules : every have the same value

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ export const getYamsScore = (dice) => {
     // Number of points for a Yams
     let yamsScore = 0;
 
+    // if all dice have the same value, return 50 points
+    const firstDieValue = dice[0];
+    const allDiceSame = dice.every(die => die === firstDieValue);
+    if (allDiceSame) {
+        return 50;
+    }
+
+    // Sum all dice values
     for (const die of dice) {
         yamsScore += die;
     }
--- index.test.js
+++ index.test.js
@@ -2,7 +2,11 @@ import { describe, expect, it } from "vitest";
 import { getYamsScore } from "./index.js";
 // TODO: yams first tests
 describe("yams", () => {
-  it("should return the correct score", () => {
-    expect(getYamsScore([1, 2, 3, 4, 5])).toBe(15);
-  });
+    it("should return the correct score", () => {
+        expect(getYamsScore([1, 2, 3, 4, 5])).toBe(15);
+    });
+
+    it("should return 50 for a Yams", () => {   
+        expect(getYamsScore([6, 6, 6, 6, 6])).toBe(50);
+    });
 });
